190d76d3a9645cf41462030c76b707ead4dc3d0e
Pedro-AndradeS <pedro9446@gmail.com>
2020-12-21 01:57:46 -0300

    feat: create header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  grid-area: header;
+  background-color: green;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Title = styled.h1`
+  margin-left: 5%;
+`
+
+export const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  width:20%;
+  margin-right: 20px;
+`
+
+export const ProfilePicture = styled.img`
+  border-radius: 50%;
+  height: 40px;
+  margin-right:10px;
+`
+
+export const ProfileName = styled.p`
+  font-size: 18px;
+  font-weight:bold
+`
