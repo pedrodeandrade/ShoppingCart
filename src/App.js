ce7f60b36537f31815acb46f0030cde3b6e0cf28
Pedro-AndradeS <pedro9446@gmail.com>
2020-12-21 00:17:01 -0300

    feat: apply global styles and toastify container to aplication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { ToastContainer } from 'react-toastify'
+
+import ShoppingCart from '~/pages/shopping-cart'
+
+import GlobalStyle from './styles/global'
 
 function App () {
-  return <h1>Hello Codeminer</h1>
+  return <>
+      <ShoppingCart/>
+      <GlobalStyle/>
+      <ToastContainer/>
+    </>
 }
 
 export default App
