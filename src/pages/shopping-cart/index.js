c7081a7fdd582a09bbb6884337bc3a7b09f39438
Pedro-AndradeS <pedro9446@gmail.com>
2020-12-21 02:07:05 -0300

    feat: create shopping screen structure

---
+++ src/pages/shopping-cart/index.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
+
+import { getProducts } from '~/controllers/products'
+
+import Header from '~/components/Header'
+
+import { showErrorAlert } from '~/utils/functions/alerts'
+
+import { Container, ProductsContainer } from './styles'
+
+function ShoppingCart () {
+  const [products, setProducts] = useState([])
+
+  useEffect(async () => {
+    const response = await getShopProducts()
+
+    if (response)
+      setProducts(response)
+  }, [])
+
+  async function getShopProducts () {
+    try {
+      const response = await getProducts()
+
+      return response
+    } catch (error) {
+      showErrorAlert('A error happened when trying to load the products, please refresh the page')
+    }
+  }
+
+  return (
+    <Container>
+      <Header/>
+      <ProductsContainer/>
+    </Container>
+  )
+}
+
+export default ShoppingCart
---
+++ src/pages/shopping-cart/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: grid;
+  width: 100%;
+  height: 100%;
+  background-color: red;
+  grid-template-columns: 3fr 1fr;
+  grid-template-rows: 10% auto;
+  grid-template-areas:
+    "header header"
+    "products cart";
+`
+
+export const ProductsContainer = styled.div`
+  grid-area: products;
+  background-color: blue;
+`
