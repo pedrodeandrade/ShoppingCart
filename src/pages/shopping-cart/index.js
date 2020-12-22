0fb1d6aeb5637da5c28ee9730f31704ecd41c228
Pedro-AndradeS <pedro9446@gmail.com>
2020-12-22 13:26:46 -0300

    feat: add shopping cart and context to shopping cart page

--- src/pages/shopping-cart/index.js
+++ src/pages/shopping-cart/index.js
@@ -1,19 +1,38 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+
+import ShoppingCartContext from '~/contexts/ShoppingCartContext'
 
 import { getProducts } from '~/controllers/products'
 
 import Header from '~/components/Header'
 import ProductCard from '~/components/ProductCard'
+import ShoppingCartProductItem from '~/components/ShoppingCartProductItem'
 
 import { showErrorAlert } from '~/utils/functions/alerts'
+import { formatNumberToUSD } from '~/utils/functions/currency'
 
-import { Container, ProductsContainer } from './styles'
+import {
+  Container,
+  ProductsContainer,
+  ShoppingCartContainer,
+  ShoppingCartTab,
+  ShoppingCartTitleContainer,
+  ShoppingCartTitle,
+  ShoppingCartProductsContainer,
+  ShoppingCartDataContainer,
+  ShoppingCartDataLabel,
+  ShoppingCartDataValue
+} from './styles'
 
 function ShoppingCart () {
   const [products, setProducts] = useState([])
 
+  const { initializeShoppingCart, shoppingCart } = useContext(ShoppingCartContext)
+
   useEffect(async () => {
+    initializeShoppingCart()
+
     const response = await getShopProducts()
 
     if (response)
@@ -26,18 +45,58 @@ function ShoppingCart () {
 
       return response
     } catch (error) {
-      showErrorAlert('A error happened when trying to load the products, please refresh the page')
+      showErrorAlert('A error happened when trying to load the products, please refresh the page!')
     }
   }
+
+  const renderShoppingCartItem = (label, value) => (
+    <ShoppingCartDataContainer>
+      <ShoppingCartDataLabel>
+        {label}
+      </ShoppingCartDataLabel>
+      <ShoppingCartDataValue>
+        {formatNumberToUSD(value)}
+      </ShoppingCartDataValue>
+    </ShoppingCartDataContainer>
+  )
 
   return (
     <Container>
       <Header/>
       <ProductsContainer>
+        <ul>
         {
-          products ? products.map(product => <ProductCard product={product} key={product.id.toString()}/>) : null
+          products ? products.map(product => <li key={product.id.toString()}><ProductCard product={product}/> </li>) : null
         }
+        </ul>
       </ProductsContainer>
+      <ShoppingCartContainer>
+        <ShoppingCartTab>
+          <ShoppingCartTitleContainer>
+            <ShoppingCartTitle>
+              Shopping Cart
+            </ShoppingCartTitle>
+          </ShoppingCartTitleContainer>
+          <ShoppingCartProductsContainer>
+            {shoppingCart
+              ? shoppingCart.map(({ id }) => <ShoppingCartProductItem key={id.toString()} productId={id}/>)
+              : null
+            }
+          </ShoppingCartProductsContainer>
+          {
+            renderShoppingCartItem('Subtotal', 20)
+          }
+          {
+            renderShoppingCartItem('Shipping', 10)
+          }
+          {
+            renderShoppingCartItem('Discount', 30)
+          }
+          {
+            renderShoppingCartItem('Total', 10)
+          }
+        </ShoppingCartTab>
+      </ShoppingCartContainer>
     </Container>
   )
 }
--- src/pages/shopping-cart/styles.js
+++ src/pages/shopping-cart/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: red;
-  grid-template-columns: 3fr 1fr;
+  grid-template-columns: 2fr 2fr;
   grid-template-rows: 10% auto;
   grid-template-areas:
     "header header"
@@ -14,9 +14,68 @@ export const Container = styled.div`
 
 export const ProductsContainer = styled.div`
   grid-area: products;
-  background-color: blue;
+  background-color: white;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  justify-content: center;
+
+  ul {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 20px;
+  }
+`
+export const ShoppingCartContainer = styled.div`
+  grid-area: cart;
+  background: white;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+`
+export const ShoppingCartTab = styled.div`
+  display:grid;
+  grid-template-rows: 10% 50% 10% 10% 10% 10%;
+  background-color: #D8D8D8; 
+  border-radius: 2%;
+  height: 80%;
+  width: 50%; 
+  margin-right: 10%;
+`
+
+export const ShoppingCartTitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #BBBBBB;
+`
+
+export const ShoppingCartTitle = styled.p`
+  font-weight: bold;
+  font-size: 20px;
+  color: #333333;
+`
+export const ShoppingCartProductsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const ShoppingCartDataContainer = styled.div`
+  display: flex;
+  flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+  padding: 0 3%;
+  border-top: 1px solid #BBBBBB;
+  margin-top: 3%;
+`
+export const ShoppingCartDataLabel = styled.p`
+  font-size: 20px;
+  color: #333333;
+`
+export const ShoppingCartDataValue = styled.p`
+  font-size: 15px;
+  color: #333333;
 `
